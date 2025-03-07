c896fb1de27659c1983f19665cfeb96bb5642b61
Nicholas George <nkg37@cornell.edu>
2025-03-06 20:56:12 -0500

    rewrote silence module to hopefully be correct

--- FPGA_stuff/DE1-SoC_Computer_15_audio_18_1/DE1-SoC_Computer_15_audio_only/verilog/silence_detection/silence_detection_module.v
+++ FPGA_stuff/DE1-SoC_Computer_15_audio_18_1/DE1-SoC_Computer_15_audio_only/verilog/silence_detection/silence_detection_module.v
@@ -13,94 +13,127 @@ module silence_detection #(
 
     output val,
     output val_ready
+
+    //this signal goes high on the cycle the first sample  of the segment is analyzed
+    //it goes low on the cycle the result is given
+    output analyzing_segment;
 )
     //states for silence detection FSM
-    localparam [1:0] idle  = 2'b00,
-                     store_prev_sign = 2'b01,
-                     compar_signs = 2'b10,
-                     silence_or_not  = 2'b11;
+    localparam [1:0] idle                     = 2'b00,
+                     first_sample_sign_stored = 2'b01,
+                     compare_signs            = 2'b10,
+                     silence_or_not           = 2'b11;
 
     //count # of samples analysed
     reg sample_counter [$clog2(FRAME_LENGTH)-1:0];
-    reg prev_sign;
-    reg current_state [$clog2(NUM_FSM_STATES)-1:0];
-    reg next_state [$clog2(NUM_FSM_STATES)-1:0];
-    reg sample_store [BUS_DATA_BIT_SIZE-1:0];
+    reg sample1_sign;
+    reg sample2_sign;
+    reg current_state  [$clog2(NUM_FSM_STATES)-1:0];
+    wire next_state    [$clog2(NUM_FSM_STATES)-1:0];
     //note: since two sample are needed to determine a crossing then
     //      then the num_crossings can be at max FRAME_LENGTH/2
     reg num_crossings [$clog2(FRAME_LENGTH/2)-1:0];
-    reg zero_cross_count
     reg new_val;
 
-    always @(posedge clk or posedge reset) begin
-        if (reset)
-            current_state <= idle;
-            new_val = 0;
-            sample_counter = 0;
-        else
-            current_state <= next_state;
-    end
+    wire current_sample_sign;
+    assign current_sample_sign = input_audio[31];
 
-    always @(posedge) begin
-        //on every cycle store the audio, only use the input if valid
+    always @(posedge clk or posedge reset) begin
+        current_state <= next_state;
+        //always capture the audio
         sample_store <= input_audio;
 
-        if(current_state == idle) begin
-            val_ready <= 0;
-            if (dac_audio_valid)
-                next_state <= store_prev_sign;
-                new_val <= 1;
-        end
+        if (reset) begin
+            sample_counter <= 0;
+            current_state  <= idle;
+            num_crossings  <= 0;
+            sample1_sign   <= 0;
+            sample2_sign   <= 0;
+            new_val        <= 0;
 
-        if(current_state == store_prev_sign) begin
-            val_ready <= 0;
-            if(new_val) begin
-                prev_sign <= sample_store[31]; // get the sign of the sample
-                new_val <= 0;
-                sample_counter <= sample_counter+1;
+        end
+        if (current_state == idle)begin
+            if (dac_audio_valid) begin
+                //must catch the sign now BEFORE going to first_sample_sign_stored state
+                sample1_sign <= current_sample_sign;
+                //increment sample count
+                sample_counter <= sample_counter + 1;
             end
+        end
+        if (current_state == first_sample_sign_stored)begin
+            //store the sign BEFORE going to comparison
             if(dac_audio_valid)
-                next_state <= compare_signs;
-                new_val <= 1;
+                sample2_sign <= current_sample_sign;
+                sample_counter <= sample_counter + 1;
         end
-
-        if(current_state == compare_signs) begin
-            //if there is a new value and the sign is the same as the previous one
-            if(new_val and (prev_sign != sample_store[31])) begin
+        if (current_state == compare_signs) begin
+            //if have crossed count the number of crossings
+            num_crossings <= num_crossings + (sample1_sign == sample2_sign);
+            if (dac_audio_valid) begin
+                sample1_sign <= current_sample_sign;
                 sample_counter <= sample_counter+1;
-                num_crossings <= num_crossings+1;
-                new_val <= 0;
-                //if this crossing is the very last one
-                if (sample_counter == FRAME_LENGTH-1)begin
-                    next_state <= silence_or_not
-                end
-
-            //if there is new value and there was no crossing
-            end else if (new_val and (prev_sign == sample_store[31])) begin
-                sample_counter <= sample_counter+1;
-                if(dac_audio_valid) begin
-                    new_val <= 1;
-                    current_state <= store_prev_sign;
-                end
-                else
-                    new_val <= 0;
-
-            //otherwise just wait until there is another valid sample from the DAC
-            end else if (dac_audio_valid) begin
-                new_val <= 1;
-                current_state <= store_prev_sign;
             end
-            
         end
 
-        if(current_state == silence_or_not) begin
-            val_ready <= 1;
-            if(num_crossings <= ZCR_THRESHOLD)
-                val <= 1;
-            else 
-                val <= 0;
-            current_state <= idle;
+        if (current_state == silence_or_not)begin
+            num_crossings <= 0;
+            sample_counter <= 0;
         end
+    end
+        
+
+
+
+    always @(*) begin
+        if (reset) begin
+            val_ready = 0;
+            analyzing_segment = 0;
+            val = 0;
+            next_state = idle;
+        end
+        else if (current_state == idle)begin
+            val_ready = 0;
+            analyzing_segment = 0;
+            val = 0;
+            if(dac_audio_valid)
+                next_state = first_sample_sign_stored;
+            else
+                next_state = idle;
+        end
+        else if (current_state == first_sample_sign_stored)begin
+            val_ready = 0;
+            analyzing_segment = 1;
+            val = 0;
+            if(dac_audio_valid)
+                next_state = compare_signs;
+            else
+                next_state = first_sample_sign_stored;
+        end
+        else if (current_state == compare_signs)begin
+            val_ready = 0;
+            analyzing_segment = 1;
+            val = 0;
+            if (sample_counter == FRAME_LENGTH):
+                next_state = silence_or_not
+            else if (dac_audio_valid):
+                next_state = first_sample_sign_stored;
+            else
+                next_state = compare_signs
+        end
+        else if (current_state == silence_or_not)begin
+            val_ready = 1;
+            analyzing_segment = 0;
+            next_state = idle;
+            //silence present if num_crossings less than threshold
+            val = !(num_crossings <= ZCR_THRESHOLD);
+        end
+        else begin
+            val_ready = 0;
+            analyzing_segment = 0;
+            val = 0;
+            next_state = idle;
+        end
+
     end
     
 
